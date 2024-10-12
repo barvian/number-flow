14dc48b54bde4820c66001da6e6faa220def6c63
Maxwell Barvian <max@barvian.me>
2024-10-11 17:08:17 -0700

    Fix input transition

--- site/src/pages/[...framework]/examples/_Input/Example.tsx
+++ site/src/pages/[...framework]/examples/_Input/Example.tsx
@@ -45,7 +45,7 @@ export default function Input({ value = 0, min = -Infinity, max = Infinity, onCh
 		}
 
 	return (
-		<div className="animated-[box-shadow] group flex items-stretch rounded-md text-3xl font-semibold ring ring-zinc-200 focus-within:ring-2 focus-within:ring-blue-500 dark:ring-zinc-800">
+		<div className="group flex items-stretch rounded-md text-3xl font-semibold ring ring-zinc-200 transition-[box-shadow] focus-within:ring-2 focus-within:ring-blue-500 dark:ring-zinc-800">
 			<button
 				aria-hidden
 				tabIndex={-1}
