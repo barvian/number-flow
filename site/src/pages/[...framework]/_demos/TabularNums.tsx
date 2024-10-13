9eb20408f245b88b26aa5c5760a531d3dc827cfa
Maxwell Barvian <max@barvian.me>
2024-10-13 11:10:35 -0700

    Add tabular nums docs

---
+++ site/src/pages/[...framework]/_demos/TabularNums.tsx
@@ -0,0 +1,31 @@
+import Demo, { DemoSwitch, type DemoProps } from '@/components/Demo'
+import NumberFlow from '@number-flow/react'
+import * as React from 'react'
+
+export default function DemoHOC({ ...rest }: Omit<DemoProps, 'children' | 'code'>) {
+	const [value, setValue] = React.useState(10)
+	const [tabularNums, setTabularNums] = React.useState(false)
+
+	return (
+		<Demo
+			{...rest}
+			title={
+				<DemoSwitch checked={tabularNums} onChange={setTabularNums}>
+					<code className="font-semibold">tabular-nums</code>
+				</DemoSwitch>
+			}
+			onClick={() => setValue((v) => v + 1)}
+		>
+			<div className="~text-xl/4xl flex items-center gap-4">
+				<NumberFlow
+					style={{
+						fontVariantNumeric: tabularNums ? 'tabular-nums' : undefined,
+						'--number-flow-char-height': '0.85em'
+					}}
+					value={value}
+					className="font-semibold"
+				/>
+			</div>
+		</Demo>
+	)
+}
