0a607141e9b2fe29fd2415caf60c27f2c02049d5
Maxwell Barvian <max@barvian.me>
2024-10-14 22:35:10 -0700

    Link in <Supported />

--- site/src/components/Supported.tsx
+++ site/src/components/Supported.tsx
@@ -1,5 +1,6 @@
 import { useCanAnimate } from '@number-flow/react'
 import clsx from 'clsx/lite'
+import Link from './Link'
 
 export default function Supported() {
 	const canAnimate = useCanAnimate({ respectMotionPreference: false })
@@ -20,11 +21,16 @@ export default function Supported() {
 					role="alert"
 					className={clsx(
 						canAnimate ? 'h-11.5' : 'h-16.5',
-						'~top-4/8 fixed left-1/2 z-50 -ml-36 w-72 rounded-lg border border-amber-200 bg-amber-50 px-4 py-3 text-center text-sm text-amber-800 dark:border-amber-800/30 dark:bg-[#271202] dark:text-amber-50'
+						'~top-4/8 prose dark:prose-invert fixed left-1/2 z-50 -ml-36 w-72 rounded-lg border border-amber-200 bg-amber-50 px-4 py-3 text-center text-sm text-amber-800 dark:border-amber-800/30 dark:bg-[#271202] dark:text-amber-50'
 					)}
 				>
 					{!canAnimate ? (
-						<p>Your browser doesn't support NumberFlow animations</p>
+						<p>
+							Your browser doesn't{' '}
+							<Link href="https://caniuse.com/mdn-css_types_mod">
+								support NumberFlow’s animations
+							</Link>
+						</p>
 					) : (
 						<p>Reduce motion is on</p>
 					)}
