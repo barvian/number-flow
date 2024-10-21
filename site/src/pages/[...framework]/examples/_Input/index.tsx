20880fe9cc15e74319d922e961ca225ae180fa41
Maxwell Barvian <max@barvian.me>
2024-10-21 13:46:52 -0700

    Fix input demo margin

--- site/src/pages/[...framework]/examples/_Input/index.tsx
+++ site/src/pages/[...framework]/examples/_Input/index.tsx
@@ -2,14 +2,16 @@ import Demo, { type DemoProps } from '@/components/Demo'
 import Example from './Example'
 import * as React from 'react'
 import type { Rename } from '@/lib/types'
+import clsx from 'clsx/lite'
 
 export default function DemoHOC({
 	children,
+	className,
 	...rest
 }: Rename<Omit<DemoProps, 'children'>, 'code', 'children'>) {
 	const [value, setValue] = React.useState(0)
 	return (
-		<Demo {...rest} code={children} className="font-mac-ui">
+		<Demo {...rest} code={children} className={clsx(className, 'font-mac-ui')}>
 			<Example value={value} min={0} max={99} onChange={setValue} />
 		</Demo>
 	)
