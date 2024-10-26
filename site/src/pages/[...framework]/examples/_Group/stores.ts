e1f0946d24cf2ecde86af94ceb4a9e706b033abf
Maxwell Barvian <max@barvian.me>
2024-10-25 17:32:48 -0700

    adjust examples mt

--- site/src/pages/[...framework]/examples/_Group/stores.ts
+++ site/src/pages/[...framework]/examples/_Group/stores.ts
@@ -1,4 +1,4 @@
-import { cyclable, hydratable } from '/src/lib/stores'
+import { cyclable, hydratable } from '@/lib/stores'
 
 export const $number = hydratable(cyclable(124.23, 41.75, 2125.95))
 export const $diff = hydratable(cyclable(0.0564, -0.3912, 0.0029))
