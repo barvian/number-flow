4d34368c206e89f8f8f3017be5e03a3c9378f6a0
Maxwell Barvian <max@barvian.me>
2024-10-26 00:03:52 -0700

    Fix failing sveltekit tests

--- packages/svelte/tests/update.test.ts
+++ packages/svelte/tests/update.test.ts
@@ -3,7 +3,9 @@ import { test, expect } from '@playwright/test'
 test.skip(({ javaScriptEnabled }) => !javaScriptEnabled)
 
 test('updates correctly', async ({ page }) => {
-	await page.goto('/')
+	// Not sure why this is necessary for Chromium/Safari but I couldn't get it to work without it:
+	// https://www.reddit.com/r/sveltejs/comments/15m9jch/how_do_you_wait_for_sveltekit_to_be_completely/
+	await page.goto('/', { waitUntil: 'networkidle' })
 	const btn = page.getByRole('button', { name: 'Change' })
 	await btn.click()
 	await expect(page).toHaveScreenshot()
