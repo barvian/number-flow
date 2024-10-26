b9a9518e9cbb38f38b83e5a2a6d0575751554879
Maxwell Barvian <max@barvian.me>
2024-10-25 23:52:13 -0700

    Init Svelte, fix tests

---
+++ packages/svelte/src/lib/index.ts
@@ -0,0 +1,39 @@
+import {
+	NumberFlowLite,
+	canAnimate as _canAnimate,
+	prefersReducedMotion as _prefersReducedMotion
+} from 'number-flow'
+import { onMount } from 'svelte'
+import { derived, readable } from 'svelte/store'
+export type { Value, Format, Trend, NumberFlowLite } from 'number-flow'
+
+NumberFlowLite.define()
+
+export { default } from './NumberFlow.svelte'
+
+const canAnimate = readable(_canAnimate, (set) => {
+	onMount(() => {
+		set(_canAnimate)
+	})
+})
+
+const prefersReducedMotion = readable(false, (set) => {
+	onMount(() => {
+		set(_prefersReducedMotion?.matches ?? false)
+		const onChange = ({ matches }: MediaQueryListEvent) => {
+			set(matches)
+		}
+		_prefersReducedMotion?.addEventListener('change', onChange)
+		return () => {
+			_prefersReducedMotion?.removeEventListener('change', onChange)
+		}
+	})
+})
+
+const canAnimateWithPreference = derived(
+	[canAnimate, prefersReducedMotion],
+	([canAnimate, prefersReducedMotion]) => canAnimate && !prefersReducedMotion
+)
+
+export const getCanAnimate = ({ respectMotionPreference = true } = {}) =>
+	respectMotionPreference ? canAnimateWithPreference : canAnimate
---
+++ packages/svelte/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import NumberFlow from "$lib/index.js"
+
+    let value = 123
+</script>
+<div>
+    Text node
+    <NumberFlow {value} />
+</div>
+<button on:click={() => value = 234}>Change</button>
