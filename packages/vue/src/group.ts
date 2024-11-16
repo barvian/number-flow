1cdbcaed1d5ba179a5d557ebef13a2038631cdca
Maxwell Barvian <max@barvian.me>
2024-11-16 10:50:46 -0800

    Fix Vue type error

--- packages/vue/src/group.ts
+++ packages/vue/src/group.ts
@@ -1,9 +1,9 @@
-import type { NumberFlowLite, partitionParts } from 'number-flow'
+import type { NumberFlowLite, formatToData } from 'number-flow'
 import type { InjectionKey, Ref, ComputedRef } from 'vue'
 
 export type RegisterWithGroup = (
 	el: Ref<NumberFlowLite | undefined>,
-	parts: ComputedRef<ReturnType<typeof partitionParts>>
+	parts: ComputedRef<ReturnType<typeof formatToData>>
 ) => void
 
 export const key = Symbol() as InjectionKey<RegisterWithGroup | undefined>
