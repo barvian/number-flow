73a2e13e40b024c920fe62c5ec7d7d4495a801d7
Maxwell Barvian <max@barvian.me>
2024-11-16 06:23:42 -0800

    Suffix example

---
+++ site/src/pages/[...framework]/_demos/Suffix.tsx
@@ -0,0 +1,25 @@
+import Demo, { type DemoProps } from '@/components/Demo'
+import type { Rename } from '@/lib/types'
+import NumberFlow, { type Value } from '@number-flow/react'
+import useCycle from '@/hooks/useCycle'
+
+const NUMBERS: Value[] = [3, 25, 50]
+
+export default function DemoHOC({
+	children,
+	...rest
+}: Rename<Omit<DemoProps, 'children'>, 'code', 'children'>) {
+	const [value, cycleValue] = useCycle(NUMBERS)
+
+	return (
+		<Demo {...rest} code={children} onClick={cycleValue}>
+			<NumberFlow
+				value={value}
+				format={{ style: 'currency', currency: 'USD', maximumFractionDigits: 0 }}
+				suffix="/mo"
+				className="~text-xl/4xl font-semibold"
+				style={{ '--number-flow-char-height': '0.85em' }}
+			/>
+		</Demo>
+	)
+}
