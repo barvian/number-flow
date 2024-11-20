611e2cc7cc0c323d228f55b470afe7c2de537f6a
Maxwell Barvian <max@barvian.me>
2024-11-20 13:18:05 -0800

    Simplier max() fn

--- packages/number-flow/src/util/math.ts
+++ packages/number-flow/src/util/math.ts
@@ -1,7 +1,6 @@
 // Math.max that handles nullish numbers
 export const max = (n1?: number, n2?: number) => {
-	if (n1 != null && n2 == null) return n1
-	if (n1 == null && n2 != null) return n2
-	if (n1 != null && n2 != null) return Math.max(n1, n2)
-	return null
+	if (n1 == null) return n2
+	if (n2 == null) return n1
+	return Math.max(n1, n2)
 }
