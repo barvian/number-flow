255881d60757854133b2bd198c12332fb15723c2
Maxwell Barvian <max@barvian.me>
2024-11-22 17:31:58 -0800

    Fix site build

--- site/src/pages/[...framework]/examples/_Countdown/index.tsx
+++ site/src/pages/[...framework]/examples/_Countdown/index.tsx
@@ -1,5 +1,5 @@
 import Demo, { type DemoProps } from '@/components/Demo'
-import { $inView, $seconds } from './stores'
+import { $inView } from './stores'
 
 export default function Activity(props: DemoProps) {
 	return <Demo {...props} onIntersect={({ isIntersecting }) => $inView.set(isIntersecting)} />
