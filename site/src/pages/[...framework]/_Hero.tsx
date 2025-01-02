cd08c548c080b229409a2a5f9d7f62eaaad3cd42
Maxwell Barvian <max@barvian.me>
2025-01-02 09:11:01 -0800

    Fix typo

--- site/src/pages/[...framework]/_Hero.tsx
+++ site/src/pages/[...framework]/_Hero.tsx
@@ -68,7 +68,7 @@ export default function Hero({ sandbox }: { sandbox: string }) {
 			ref={ref}
 			className="~mb-12/24 container flex w-full max-w-2xl flex-col items-center text-center"
 		>
-			{/* "Warm up" these characters, otherwise it'll stutter the animation when the browser tries to access them the first timw */}
+			{/* "Warm up" these characters, otherwise the animation will stutter if the browser tries to access them the first time */}
 			<span className="sr-only" aria-hidden="true" {...{ inert: '' }}>
 				万米
 			</span>
