1f278754acef7863fd81a27b22e739e2fdb009c0
Maxwell Barvian <max@barvian.me>
2025-01-13 08:00:24 -0800

    Fix use within SCs

---
+++ .changeset/nasty-apples-remain.md
@@ -0,0 +1,5 @@
+---
+'@number-flow/react': patch
+---
+
+Fix use within Server Components
---
+++ packages/react/test/apps/react-19/app/sc/page.tsx
@@ -0,0 +1,5 @@
+import NumberFlow from '@number-flow/react'
+
+export default function SC() {
+	return <NumberFlow value={123} />
+}
