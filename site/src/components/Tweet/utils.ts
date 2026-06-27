82db5cac4962832420b9831d6490f7de93478e5e
Max Barvian <max@barvian.me>
2026-06-27 10:18:47 -0700

    fix tweets

--- site/src/components/Tweet/utils.ts
+++ site/src/components/Tweet/utils.ts
@@ -105,12 +105,15 @@ function getEntities(tweet: TweetBase): Entity[] {
 	const textMap = Array.from(tweet.text)
 	const result: EntityWithType[] = [{ indices: tweet.display_text_range, type: 'text' }]
 
-	addEntities(result, 'hashtag', tweet.entities.hashtags)
-	addEntities(result, 'mention', tweet.entities.user_mentions)
-	addEntities(result, 'url', tweet.entities.urls)
-	addEntities(result, 'symbol', tweet.entities.symbols)
-	if (tweet.entities.media) {
-		addEntities(result, 'media', tweet.entities.media)
+	const { hashtags = [], user_mentions = [], urls = [], symbols = [], media } =
+		tweet.entities ?? {}
+
+	addEntities(result, 'hashtag', hashtags)
+	addEntities(result, 'mention', user_mentions)
+	addEntities(result, 'url', urls)
+	addEntities(result, 'symbol', symbols)
+	if (media) {
+		addEntities(result, 'media', media)
 	}
 	fixRange(tweet, result)
 
@@ -141,9 +144,9 @@ function getEntities(tweet: TweetBase): Entity[] {
 function addEntities(
 	result: EntityWithType[],
 	type: EntityWithType['type'],
-	entities: TweetEntity[]
+	entities?: TweetEntity[] | null
 ) {
-	for (const entity of entities) {
+	for (const entity of entities ?? []) {
 		for (const [i, item] of result.entries()) {
 			if (item.indices[0] > entity.indices[0] || item.indices[1] < entity.indices[1]) {
 				continue
@@ -175,12 +178,9 @@ function addEntities(
  * Array.from is unicode aware, unlike string.slice()
  */
 function fixRange(tweet: TweetBase, entities: EntityWithType[]) {
-	if (
-		tweet.entities.media &&
-		tweet.entities.media[0] &&
-		tweet.entities.media[0].indices[0] < tweet.display_text_range[1]
-	) {
-		tweet.display_text_range[1] = tweet.entities.media[0].indices[0]
+	const media = tweet.entities?.media
+	if (media?.[0] && media[0].indices[0] < tweet.display_text_range[1]) {
+		tweet.display_text_range[1] = media[0].indices[0]
 	}
 	const lastEntity = entities.at(-1)
 	if (lastEntity && lastEntity.indices[1] > tweet.display_text_range[1]) {
